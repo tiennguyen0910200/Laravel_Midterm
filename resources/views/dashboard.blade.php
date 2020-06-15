69fc427db80f03b936d666dcd031649e936f3346
Nguyen Thi Tien <tien.nguyen21@pn.com.vn>
2020-06-15 11:38:28 +0700

    show data

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,6 +7,7 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
 	<link rel="stylesheet" type="text/css" href="/css/home.css">
 	<style type="text/css">
 		p{
@@ -25,7 +26,7 @@
 			@foreach($tours as $todo)
 			<div class="col-sm-4">
 				<div style="border: 1px solid #fef3f5; width:400px;height: 500px; margin-bottom: 30px;">
-					<center><img class="img" style="width: 300px;height: 250px;" src="{{'/storage/'.$todo->img}}"/></center>
+					<center><img class="img" style="width: 350px;height: 250px;" src="{{'/storage/'.$todo->img}}"/></center>
 					
 					<br>
 					<center><h5 class="name"><b>{{ $todo->name}}</b></h5></center>
@@ -33,12 +34,13 @@
 					<div class="container">
 						<div class="row">
 							<div class="col-sm-6">
-								<h6>Lịch trình:{{ $todo->schelude}}</h6>
-								<h6>Khởi hành:{{ $todo->depart}}</h6>
-								<h6>Số chỗ còn thêm:{{ $todo->numberPeople}}</h6>
+								<i class="far fa-compass">Hành Trình:{{$todo->schelude}}</i></h6>
+								<i class="far fa-play-circle">Khởi hành:{{$todo->depart}}</i>
+								<i class="far fa-address-book">Số chỗ còn thêm:{{ $todo->numberPeople}}</i>
+								
 							</div>
 							<div class="col-sm-6">
-								<h5 style="margin-left: 40px; color: #f6007d">{{ $todo->price}}.đ</h5>
+								<h5 style="margin-left: 40px; margin-top: 60px; color: #f6007d">{{ $todo->price}}.đ</h5>
 							</div>
 						</div>
 					</div>
